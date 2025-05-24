321180729aa8e69844b0b64048242adf99d0f028
Xhall1 <gridoracones@gmail.com>
2025-05-24 00:48:17 -0500

    feat: agregando el archivo card.java

---
+++ card/Card.java
@@ -0,0 +1,32 @@
+package card;
+
+// Representa una carta en el mazo
+public class Carta {
+    private String palo; // Palo de la carta la cuál en este caso sería: ♠, ♥, ♦, ♣
+    private String valor; // Valor de la carta cómo sería en este caso: A, 2-10, J, Q, K
+    private int puntos; // Puntos asociados a la carta
+
+    public Carta(String palo, String valor, int puntos) {
+        this.palo = palo;
+        this.valor = valor;
+        this.puntos = puntos;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    // Aquí se representa la carta como un texto
+    @Override
+    public String toString() {
+        return valor + " de " + palo;
+    }
+}
